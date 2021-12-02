26cc8d908be6fd1fc164a114922d6764ea07c479
L-j-h-c <ckrgkswnsgh@naver.com>
2021-12-02 19:05:45 +0900

    ✨[FEAT] : signUp POST로 구현하기
    signUp POST로 구현하기

---
+++ Moya-laboratory/Moya-laboratory/Network/GeneralAPI.swift
@@ -0,0 +1,12 @@
+//
+//  GeneralAPI.swift
+//  Moya-laboratory
+//
+//  Created by Junho Lee on 2021/12/02.
+//
+
+import Foundation
+
+struct GeneralAPI {
+    static let baseURL = "https://asia-northeast3-we-sopt-29.cloudfunctions.net/api"
+}
