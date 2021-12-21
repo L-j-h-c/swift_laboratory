fd482a0e56f3f81ec4a900d46ed04a8d863353c1
L-j-h-c <ckrgkswnsgh@naver.com>
2021-12-21 15:54:53 +0900

    ✨[FEAT] : animator 선언과 RunningPropertyAnimator 이용 애니메이션
    animator 선언과 RunningPropertyAnimator 이용 애니메이션 그리기

--- animation-basic/animation-basic/ViewController.swift
+++ animation-basic/animation-basic/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstUIView: UIView!
+    @IBOutlet weak var secondUIView: UIView!
+    @IBOutlet weak var thirdUIView: UIView!
+    @IBOutlet weak var fourthUIView: UIView!
+    @IBOutlet weak var fifthUIView: UIView!
+    
+    var animation: UIViewPropertyAnimator?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        firstAnimation()
+        secondAnimation()
     }
-
-
+    
+    func firstAnimation() {
+        // 방법1: 애니메이션 즉시실행
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 3, delay: 0, options: [.repeat]) { self.firstUIView.frame = .init(x: 100, y: 300, width: 150, height: 200) }
+        
+        // 방법2: startAnimation() 호출
+        let animation = UIViewPropertyAnimator(duration: 3, curve: .easeInOut) { self.secondUIView.frame = .init(x: 100, y: 300, width: 150, height: 200)
+        };
+        animation.addAnimations({
+            
+        }, delayFactor: 1)
+        animation.addAnimations {
+            self.secondUIView.frame = .init(x: 0, y: 0, width: 150, height: 200)
+        animation.startAnimation()
+        }
+        
+        animation.startAnimation()
+    }
+    
+    func secondAnimation() {
+        animation = UIViewPropertyAnimator(duration: 1.5, curve: .easeInOut) { self.thirdUIView.frame = .init(x: 100, y: 300, width: 150, height: 200) }
+        guard let animation = animation else { return }
+        animation.addAnimations { self.thirdUIView.frame = .init(x: 100, y: 500, width: 150, height: 200) }
+        animation.startAnimation()
+        DispatchQueue.main.asyncAfter(deadline: .now()+1) { animation.isReversed = true }
+        
+    }
 }
 
