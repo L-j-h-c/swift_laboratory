566d46561ec3a9ba1777d45a544326e0de207af2
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-07 15:38:07 +0900

    [Add] #11 - StructClass 프로젝트 생성
     StructClass 프로젝트 생성

---
+++ StructClass/StructClass/StructClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StructClass
+//
+//  Created by Junho Lee on 2022/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
