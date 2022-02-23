c40fd1c9d77fe543b65602f6d110c700ddbd8948
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-23 22:04:37 +0900

    [Feat] #15 - 컬렉션뷰 구현
    컬렉션뷰 구현

--- DiffableDatasoruce/DiffableDataSource/DiffableDataSource/Cell/VideoCVC.swift
+++ DiffableDatasoruce/DiffableDataSource/DiffableDataSource/Cell/VideoCVC.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+import UIKit
+
+class VideoCVC: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+  var video: Video? {
+    didSet {
+      titleLabel.text = video?.title
+      subtitleLabel.text = "\(video?.lessonCount ?? 0) lessons"
+    }
+  }
+}
--- DiffableDatasoruce/DiffableDataSource/DiffableDataSource/VideosViewController.swift
+++ DiffableDatasoruce/DiffableDataSource/DiffableDataSource/VideosViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 class VideosViewController: UICollectionViewController {
   // MARK: - Properties
+  private var videoList = Video.allVideos
   private var searchController = UISearchController(searchResultsController: nil)
+  
+  // MARK: - Value Types
+  
+  // MARK: - Life Cycles
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
@@ -21,9 +25,36 @@ class VideosViewController: UICollectionViewController {
   // MARK: - Functions
 }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+// MARK: - UICollectionViewDataSource
+extension VideosViewController {
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return videoList.count
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let video = videoList[indexPath.row]
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "VideoCVC", for: indexPath) as? VideoCVC else { fatalError() }
+    cell.video = video
+    cell.backgroundColor = .systemGray
+    return cell
+  }
+}
+
+// MARK: - UICollectionViewDelegate
+extension VideosViewController {
+//  override func collectionView(
+//    _ collectionView: UICollectionView,
+//    didSelectItemAt indexPath: IndexPath
+//  ) {
+//    let video = videoList[indexPath.row]
+//    guard let link = video.link else {
+//      print("Invalid link")
+//      return
+//    }
+//    let safariViewController = SFSafariViewController(url: link)
+//    present(safariViewController, animated: true, completion: nil)
+//  }
+}
 // MARK: - UISearchResultsUpdating Delegate
 extension VideosViewController: UISearchResultsUpdating {
     }
@@ -36,6 +67,45 @@ extension VideosViewController: UISearchResultsUpdating {
   }
 }
 
+// MARK: - Layout Handling
+extension VideosViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView,
+                     layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 210, height: 57)
+  }
+              
+  func collectionView(_ collectionView: UICollectionView,
+                     layout collectionViewLayout: UICollectionViewLayout,
+                      insetForSectionAt section: Int) -> UIEdgeInsets {
+    return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+  }
+}
 
+// MARK: - Layout Handling
+extension VideosViewController {
+//  private func configureLayout() {
+//    collectionView.collectionViewLayout = UICollectionViewCompositionalLayout(sectionProvider: { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+//      let isPhone = layoutEnvironment.traitCollection.userInterfaceIdiom == UIUserInterfaceIdiom.phone
+//      let size = NSCollectionLayoutSize(
+//        widthDimension: NSCollectionLayoutDimension.fractionalWidth(1),
+//        heightDimension: NSCollectionLayoutDimension.absolute(isPhone ? 70 : 250)
+//      )
+//      let itemCount = isPhone ? 2 : 3
+//      let item = NSCollectionLayoutItem(layoutSize: size)
+//      let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitem: item, count: itemCount)
+//      let section = NSCollectionLayoutSection(group: group)
+//      section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+//      section.interGroupSpacing = 10
+//      return section
+//    })
+//  }
+  
+  override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    super.viewWillTransition(to: size, with: coordinator)
+    coordinator.animate(alongsideTransition: { context in
+      self.collectionView.collectionViewLayout.invalidateLayout()
+    }, completion: nil)
+  }
 }
 
