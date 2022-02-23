09f918dbe28c3958d2cb96d913d898c3c2b1eeae
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-23 22:02:52 +0900

    [Study] #15 - Search Controller UI 세팅
    Search Controller UI 세팅

--- DiffableDatasoruce/DiffableDataSource/DiffableDataSource/VideosViewController.swift
+++ DiffableDatasoruce/DiffableDataSource/DiffableDataSource/VideosViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+class VideosViewController: UICollectionViewController {
+  // MARK: - Properties
+  private var searchController = UISearchController(searchResultsController: nil)
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    configureSearchController()
+//    configureLayout()
+  }
+  
+  // MARK: - Functions
+}
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+// MARK: - UISearchResultsUpdating Delegate
+extension VideosViewController: UISearchResultsUpdating {
     }
+  func configureSearchController() {
+    searchController.searchResultsUpdater = self
+    searchController.obscuresBackgroundDuringPresentation = false
+    searchController.searchBar.placeholder = "Search Videos"
+    navigationItem.searchController = searchController
+    definesPresentationContext = true
+  }
+}
 
 
 }
