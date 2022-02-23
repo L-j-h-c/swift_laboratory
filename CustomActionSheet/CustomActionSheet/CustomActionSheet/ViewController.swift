e77b80976d4c471adfa34c8ada8525b0f30978c0
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-23 18:51:29 +0900

    [Feat] #14 - 화면전환 기능 구현
    화면전환 기능 구현

--- CustomActionSheet/CustomActionSheet/CustomActionSheet/ViewController.swift
+++ CustomActionSheet/CustomActionSheet/CustomActionSheet/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let customAction = SparkActionSheet()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUI()
     }
-
-
+    lazy var backgroundView: UIView = {
+        let backgroundView = UIView()
+        backgroundView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        backgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        return backgroundView
+    }()
+    
+    let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapGestureDidRecognize(_:)))
+    
+    @IBAction func presentActionSheet(_ sender: Any) {
+        present(customAction, animated: true)
+    }
+    
+    private func setUI() {
+        view.backgroundColor = .systemPurple
+        
+        lazy var cancelButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 300))
+        cancelButton.setTitleColor(.red, for: UIControl.State())
+        cancelButton.setTitle("cancelView", for: UIControl.State())
+        cancelButton.isUserInteractionEnabled = true
+        cancelButton.addGestureRecognizer(tapRecognizer)
+        cancelButton.addTarget(self, action: #selector(tapGestureDidRecogniz), for: .touchUpInside)
+        cancelButton.isEnabled = true
+        view.addSubview(cancelButton)
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+    @objc func tapGestureDidRecognize(_ gesture: UITapGestureRecognizer) {
+        print("hi")
+//        self.dismiss(animated: true)
+    }
+    
+    @objc func tapGestureDidRecogniz() {
+        print("hi")
+//        self.dismiss(animated: true)
+    }
 }
 
