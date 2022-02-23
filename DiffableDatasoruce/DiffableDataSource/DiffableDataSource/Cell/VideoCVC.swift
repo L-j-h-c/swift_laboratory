ca4105ea33ea177a99b97c9e6574078e431c9ab4
L-j-h-c <ckrgkswnsgh@naver.com>
2022-02-23 22:01:00 +0900

    [Chore] #15 - 프로젝트 폴더링
    프로젝트 폴더링

---
+++ DiffableDatasoruce/DiffableDataSource/DiffableDataSource/Cell/VideoCVC.swift
@@ -0,0 +1,8 @@
+//
+//  VideoCVC.swift
+//  DiffableDataSource
+//
+//  Created by Junho Lee on 2022/02/23.
+//
+
+import Foundation
---
+++ DiffableDatasoruce/DiffableDataSource/DiffableDataSource/Model/Video.swift
@@ -0,0 +1,8 @@
+//
+//  Video.swift
+//  DiffableDataSource
+//
+//  Created by Junho Lee on 2022/02/23.
+//
+
+import Foundation
