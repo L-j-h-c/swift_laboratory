4c6effa459da2edd1bfb69a6b35d2a58149e5979
L-j-h-c <ckrgkswnsgh@naver.com>
2022-06-01 22:58:29 +0900

    [Feat] #23 - request 익스텐션 만들고 뷰컨에서 호출하기

--- LocalNotification/LocalNotification/LocalNotification/UNNotificationCenter+.swift
+++ LocalNotification/LocalNotification/LocalNotification/UNNotificationCenter+.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import UserNotifications
+
+extension UNUserNotificationCenter {
+    func addNotificationRequest(by date: DateComponents, id: String) {
+        
+        // content 만들기
+        let content = UNMutableNotificationContent()
+        content.title = "알림 제목입니다"
+        content.body = "알림 바디입니다. 여기 내용이 들어갑니다."
+        content.sound = .default
+        content.badge = 1
+        
+        // trigger 만들기
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+        
+        // request 만들기
+        let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
+        
+        self.add(request, withCompletionHandler: nil)
+    }
+}
