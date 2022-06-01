49ca4023aa0156b7369a9e0aa55997e24edcc7bd
L-j-h-c <ckrgkswnsgh@naver.com>
2022-06-01 22:58:54 +0900

    [Feat] #23 - 뷰컨에서 호출하고 SceneDeleagte에서 지워주기

--- LocalNotification/LocalNotification/LocalNotification/ViewController.swift
+++ LocalNotification/LocalNotification/LocalNotification/ViewController.swift
@@ -12,8 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .green
+        addCalendarNoti()
+//        removeCalendarNoti()
     }
 
 
+    private func addCalendarNoti() {
+        
+        let calendar = Calendar.current
+        let newDate = calendar.date(byAdding: DateComponents(second: 5), to: .now)
+        let components = calendar.dateComponents([.hour, .minute, .second], from: newDate!)
+        UNUserNotificationCenter.current().addNotificationRequest(by: components, id: UUID().uuidString)
+    }
+    
+    private func removeCalendarNoti() {
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+    }
 }
 
