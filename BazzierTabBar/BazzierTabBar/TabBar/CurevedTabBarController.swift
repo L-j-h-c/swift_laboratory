36df6a8af72246c75f1a3f5e072bc85c2937badb
L-j-h-c <ckrgkswnsgh@naver.com>
2022-09-14 13:40:48 +0900

    [Chore] #31 - CurvedTabBar 추가

--- BazzierTabBar/BazzierTabBar/TabBar/CurevedTabBarController.swift
+++ BazzierTabBar/BazzierTabBar/TabBar/CurevedTabBarController.swift
@@ -7,23 +7,23 @@
 
 import UIKit
 
-class CurevedTabBarController: UITabBarController {
+class CurevedTabBarController: UITabBarController, UITabBarControllerDelegate {
 
+    let curvedTabbar = CurvedTabBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTabBar()
+        setDelegate()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension CurevedTabBarController {
+    private func setTabBar() {
+        self.tabBar
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setDelegate() {
+        self.delegate = self
     }
-    */
-
 }
