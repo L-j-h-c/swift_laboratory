0b2c8080d5457f9f8936869670b54ee3d85595cc
L-j-h-c <ckrgkswnsgh@naver.com>
2022-09-15 20:31:31 +0900

    [Add] TuistTest 생성

---
+++ TuistTest/Targets/TuistTest/Sources/AppDelegate.swift
@@ -0,0 +1,25 @@
+import UIKit
+import TuistTestKit
+import TuistTestUI
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .white
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        TuistTestKit.hello()
+        TuistTestUI.hello()
+
+        return true
+    }
+
+}
