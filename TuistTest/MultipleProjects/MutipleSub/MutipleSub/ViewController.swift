2124ae7bcf213fa2d043d88d500308b0efd7cdfd
L-j-h-c <ckrgkswnsgh@naver.com>
2022-09-18 14:33:02 +0900

    [Add] #34 - MultipleProjects 생성

---
+++ TuistTest/MultipleProjects/MutipleSub/MutipleSub/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MutipleSub
+//
+//  Created by Junho Lee on 2022/09/18.
+//
+
+import UIKit
+
+open class CustomViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    public func hello() {
+        print("hello")
+    }
+}
+
